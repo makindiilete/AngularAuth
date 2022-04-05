e0ca25a5e99fcfdf03435588fa3628cff61f37af
Michael Akindilete <michael.akindilete@MichaelAkindeletes-MacBook-Pro.local>
2022-04-05 20:12:13 +0100

    updates

---
+++ server/routes/api.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/users");
+
+const mongoose = require("mongoose");
+
+const db =
+  "mongodb+srv://makindiilete:123999abc@angularauth.xph1z.mongodb.net/eventsdb?retryWrites=true&w=majority";
+
+mongoose.connect(db, (err) => {
+  console.log(`Connecting....`);
+  if (err) {
+    console.error(`Error! ${err}`);
+  } else {
+    console.log(`Connected to mongodb`);
+  }
+});
+router.get("/", (req, res) => {
+  res.send("From API route");
+});
+
+router.post("/register", (req, res) => {
+  let userData = req.body;
+  // create a new user using d req body
+  let user = new User(userData);
+
+  user.save((error, registeredUser) => {
+    if (error) console.log(error);
+    else {
+      res.status(200).send(registeredUser);
+    }
+  });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const PORT = 5555;
+const api = require("./routes/api");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api", api); //localhost:5555/api
+
+app.get("/", function (req, res) {
+  res.send("Hello from server");
+});
+
+app.listen(PORT, function () {
+  console.log(`Server running on port ${PORT}`);
+});
